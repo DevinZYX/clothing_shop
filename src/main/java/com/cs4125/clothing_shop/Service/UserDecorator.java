6014750f59365ad05261d45f77248f65bcc964dd
ULxzj <22156453@studentmail.ul.ie>
2023-11-15 15:01:34 +0000

    UserDecorator first version

--- src/main/java/com/cs4125/clothing_shop/Service/UserDecorator.java
+++ src/main/java/com/cs4125/clothing_shop/Service/UserDecorator.java
@@ -11,13 +11,98 @@ public abstract class UserDecorator extends User {
     public UserDecorator(User decoratedUser){
         this.decoratedUser = decoratedUser;
     }
-
-//    @Override
-//    public double getDiscount(){
-//        if(decoratedUser.getLevel().equals(UserLevel.BRONZE_USER)){
-//            return 0.95;
-//        }
-//    }
-
-
+    @Override
+    public Integer getId() {
+        return decoratedUser.getId();
+    }
+    @Override
+    public String getLevel() {
+        return decoratedUser.getLevel();
+    }
+    
+    
+    //RegularMember
+    public class RegularMemberDecorator extends UserDecorator {
+        private double totalPurchaseAmount = 0;
+    
+        public RegularMemberDecorator(User decoratedUser) {
+            super(decoratedUser);
+        }
+    
+        @Override
+        public double getDiscount() {
+            return super.getDiscount() * 0.95; 
+        }
+    
+        @Override
+        public void addPurchaseAmount(double amount) {
+            totalPurchaseAmount += amount;
+        }
+    
+        @Override
+        public String upgradeMembershipIfNeeded() {
+            if (totalPurchaseAmount >= 10000) {
+                decoratedUser.setLevel("Silver");
+                return "Silver";
+            }
+            return "Regular";
+        }
+    }
+    
+    //SilverMember
+    public class SilverMemberDecorator extends UserDecorator {
+        private double totalPurchaseAmount = 0;
+    
+        public SilverMemberDecorator(User decoratedUser) {
+            super(decoratedUser);
+        }
+    
+        @Override
+        public double getDiscount() {
+            return super.getDiscount() * 0.9; // 9折优惠
+        }
+    
+        @Override
+        public void addPurchaseAmount(double amount) {
+            totalPurchaseAmount += amount;
+        }
+    
+        @Override
+        public String upgradeMembershipIfNeeded() {
+            if (totalPurchaseAmount >= 50000) {
+                decoratedUser.setLevel("Gold");
+                return "Gold";
+            }
+            return "Silver";
+        }
+    }
+    
+    //GoldMember
+    public class GoldMemberDecorator extends UserDecorator {
+        private double couponAmount = 0;
+    
+        public GoldMemberDecorator(User decoratedUser) {
+            super(decoratedUser);
+        }
+    
+        @Override
+        public double getDiscount() {
+            return super.getDiscount() * 0.8;
+        }
+    
+        @Override
+        public void addPurchaseAmount(double amount) {
+            couponAmount += (int) (amount / 1000) * 10;
+        }
+    
+        @Override
+        public String upgradeMembershipIfNeeded() {
+            //No need to upgraded
+            return "Gold";
+        }
+    
+        public double getCouponAmount() {
+            return couponAmount;
+        }
+    }
 }
